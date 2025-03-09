655d030767e374f7bcb404dd3178a2f0915a75f2
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-03-08 22:55:36 -0300

    [FEAT]: tags

--- src/components/Galeria/Tags/index.jsx
+++ src/components/Galeria/Tags/index.jsx
@@ -1,8 +1,51 @@
+import tags from "./tags.json"
+import styled from "styled-components"
+
+const TituloTags = styled.h2`
+    color: #d9d9d9;
+    font-size: 24px;
+    font-weight: 400;
+`
+
+const TagsContainer = styled.section`
+    display: flex;
+    flex-direction: row;
+    margin-top: 30px;
+    gap: 36px;
+`
+
+const TagsFiltro = styled.div`
+    display: flex;
+    gap: 12px;
+    font-size: 24px;
+    font-weight: 400;
+    button {
+        background: rgba(217, 217, 217, 0.3);
+        border: 2px solid transparent;
+        border-radius: 10px;
+        color: #ffffff;
+        margin: 10px;
+        padding: 10px;
+        cursor: pointer;
+        &:hover {
+            border-color: #c98cf1;
+        }
+    }
+`
 
 const Tags = () => {
-  return (
-    <div>Tags</div>
-  )
+    return (
+        <>
+            <TagsContainer>
+                <TituloTags>Busque por tags:</TituloTags>
+                <TagsFiltro>
+                    {tags.map((tag) => (
+                        <button key={tag.id}>{tag.titulo}</button>
+                    ))}
+                </TagsFiltro>
+            </TagsContainer>
+        </>
+    )
 }
 
 export default Tags
